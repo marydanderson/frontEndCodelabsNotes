d128d155d0b8545d0bbd4d84716dd364711fe2e8
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-02-23 14:29:51 -0500

    lesson 4

---
+++ CO4_DOM/videos/Pricing Calc/app.js
@@ -0,0 +1,32 @@
+//grab what we need from the DOM
+    //input for price, qty, and div input where total is shown
+const priceInput = document.querySelector('[name=price]');
+const quantityInput = document.querySelector('[name=quantity]');
+const total = document.querySelector('.total');
+const quantityLabel = document.querySelector('.qty-label')
+
+//create functions we need
+function calculatePieCost() {
+    const price = priceInput.value;
+    const quantity = quantityInput.value;
+    
+
+    const cost = price * quantity;
+    console.log(cost)
+    total.innerText = '$' + cost.toFixed(2);
+}
+
+function updateQuantityLabel() {
+    const quantity = quantityInput.value;
+    quantityLabel.innerText = quantity;
+}
+
+//on first run
+calculatePieCost();
+
+//add event listeners
+priceInput.addEventListener('input', calculatePieCost);
+quantityInput.addEventListener('input', calculatePieCost);
+quantityInput.addEventListener('input', updateQuantityLabel);
+
+
