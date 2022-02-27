ef8ae078eb9eeebf1cdb879100256308d7b464ee
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-02-27 14:33:03 -0500

    class 5 rev

--- CO5_Bookmarking/Bookmark Ap/app.js
+++ CO5_Bookmarking/Bookmark Ap/app.js
@@ -17,7 +17,30 @@ function closeFloater() {
 }
 
 
-
 // =========     EVENT LISTENERS
 input.addEventListener('focus', showFloater);//focus is better listener than CLICK b/c it includes if you tab in there
 overlay.addEventListener('click', closeFloater);
+
+
+// ================== ADD BOOKMARKS
+// selectors
+const bookmarksList = document.querySelector('.bookmarks-list');
+const bookmarkForm = document.querySelector('.bookmark-form');
+const bookmarkInput = bookmarkForm.querySelector('input[type=text]');
+
+
+//functions
+function createBookmark(e) {
+    e.preventDefault(); //prevent page from refreshing when form is submitted
+    const title = bookmarkInput.value;
+    const bookmark = document.createElement('a');
+    bookmark.className = 'bookmark';
+    bookmark.innerText = title;
+    bookmark.href = '#';
+    bookmark.trget = 'blank' //bookmark opens in new tab
+    bookmarksList.appendChild(bookmark);
+    bookmarkForm.reset();
+}
+
+// event listeners
+bookmarkForm.addEventListener('submit', createBookmark);
