06b85265dd3019ba540b3f6f8cf560fdca18b4ee
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-02-28 21:40:03 -0500

    exercise

---
+++ CO5_Bookmarking/InClass/app.js
@@ -0,0 +1,27 @@
+// Aim: Create an application that prints out users information on submission of a form and saves the results to local storage
+
+// =========== HTML ELEMENT SELECTORS ================
+const firstNameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const ageInput = document.querySelector("#age")
+
+console.log(firstNameInput);
+console.log(lastNameInput);
+console.log(ageInput)
+
+
+
+// =========== GLOBAL VARIABLES ================
+
+
+
+// =========== FUNCTION DECLARATIONS ================
+
+
+
+// =========== EVENT LISTENERS ================
+//   click event submitting submitting for values (first name, last name, and age) to create new user object
+
+
+
+// EVENTUAL: create a 'user' tile for each user name and list at bottom of the page; when click on it, retrieve their info
