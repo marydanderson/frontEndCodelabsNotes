73981461cdf0c88d5960a3e78cc482b538a45d03
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-03-18 14:18:53 -0500

    Typescript

--- C10_Typscript/basics.ts
+++ C10_Typscript/basics.ts
@@ -39,3 +39,40 @@ let course = "The complete guide"; //typescript infers that course can only be a
 
 
 // Function Types, parameters
+//can also set functions to types and they can return a type, too; sometimes they will infer if you assign type in parameter
+function typeFunction(a: number) {
+    return a //will return a number
+}
+
+
+//Generics
+// when you make a dynamic function you might want to use later, but want it to have defined types but don't want to lock down the type based on use, use generics. 
+//bassicaly tells typscript when to expect it willl need to look for a certain type
+
+function insert<T>(array: T[], value: T) { //value needs to be same as T
+    const newArray = [value, ...array];
+    return newArray; //this value will be the same as T
+}
+
+//helps you write functions that are type safe, yet flexible
+
+// CLASSES
+
+class Student {
+    constructor(
+        public first: string, 
+        last: string, 
+        private courses: string[]
+    ){}
+
+    enroll(courseName: string) {
+        this.courses.push(courseName);
+    }
+
+    listCourse() {
+        return this.courses.slice(); //creates a copy of the course
+    }
+}
+
+const studentOne = new Student('mary', 'anderson', ['courese 1']);
+studentOne.enroll("course 2");
