7e6146f05a8d7c9338166a97da888f3ff70aa2bd
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-03-21 21:15:52 -0400

    Intro to angular exercises
    completed all in class exercises

---
+++ C11_AngularIntro/InClass_Exercises/manually-create-component/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'manually-create-component';
+  home = "I'm home!";
+}
