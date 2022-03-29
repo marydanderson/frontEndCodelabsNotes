5e69c4cdad8bb953929c954a2e18b1b98c57b89f
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-03-29 11:53:25 -0400

    c13_in class exercises

---
+++ C13_AngRecipeProject/InClass_Exercises/basic-event-binding-exercise/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-event-binding-exercise';
+
+  logToConsole() {
+    console.log("random message!")
+  }
+}
---
+++ C13_AngRecipeProject/InClass_Exercises/basic-ngmodel-exercise/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-ngmodel-exercise';
+  header: string = "header";
+  name: string = "mary"
+}
