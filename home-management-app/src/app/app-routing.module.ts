a694461cbff4952abfb2f5705b5bfceadd5acd06
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-04-12 12:53:33 -0400

    d

--- home-management-app/src/app/app-routing.module.ts
+++ home-management-app/src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeSummaryComponent } from "./home-summary/home-summary.component";
 import { MaintenanceListComponent } from "./maintenance/maintenance-list/maintenance-list.component";
-import { ProjectItemComponent } from "./project/project-item/project-item.component";
 import { ProjectListComponent } from "./project/project-list/project-list.component";
-
+import { ProjectDetailComponent } from "./project/project-detail/project-detail.component";
 
 
 const appRoutes: Routes = [
@@ -15,7 +14,7 @@ const appRoutes: Routes = [
   {
     path: "projects", component: ProjectListComponent,
     children: [
-      {path: "", component: ProjectItemComponent}
+      {path: "", component: ProjectDetailComponent}
     ]
   },
 ]
--- home-management-app/src/app/app.module.ts
+++ home-management-app/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaintenanceItemComponent } from './maintenance/maintenance-item/maintenance-item.component';
 import { MaintenanceListComponent } from './maintenance/maintenance-list/maintenance-list.component';
-import { ProjectItemComponent } from './project/project-item/project-item.component';
 import { ProjectListComponent } from './project/project-list/project-list.component';
 import { FinancialSummaryComponent } from './financials/financial-summary/financial-summary.component';
 import { RoomComponent } from './rooms/room/room.component';
@@ -13,6 +12,7 @@ import { RoomCarouselComponent } from './rooms/room-carousel/room-carousel.compo
 import { ProjectWishComponent } from './project/project-wish/project-wish.component';
 import { HomeSummaryComponent } from './home-summary/home-summary.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
 
 @NgModule({
   declarations: [
@@ -20,8 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
     NavbarComponent,
     MaintenanceItemComponent,
     MaintenanceListComponent,
-    ProjectItemComponent,
     ProjectListComponent,
+    ProjectDetailComponent,
     FinancialSummaryComponent,
     RoomComponent,
     RoomCarouselComponent,
