a7a93cee20d79ea64ab27de189039fa9d8f9a5c6
marydanderson <43974056+marydanderson@users.noreply.github.com>
2022-05-09 20:33:13 -0400

    Update server.js

--- home-management-app/server.js
+++ home-management-app/server.js
@@ -5,11 +5,11 @@ const express = require("express");
 const app = express();
 
 //Serve static build files from the "dist" directory
-app.use(express.static("./dist/home-app"));
+app.use(express.static("./dist/home-manager-app"));
 
 // Route incoming server request to the correct files
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/home-app" })
+  res.sendFile("index.html", { root: "dist/home-manager-app" })
 });
 
 //Sstart the app on the default Heroku port
